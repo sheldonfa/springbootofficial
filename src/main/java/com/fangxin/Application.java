30c9ff412b866a811ef370e7290c27e900f3ba0c
fangxin <xfang@ling-ban.com>
2018-01-10 16:26:10 +0800

    feature15 @Configuration

--- src/main/java/com/fangxin/Application.java
+++ src/main/java/com/fangxin/Application.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 1 main函数一般放在根目录
- * 2 放在根目录的main函数定义ComponentScan不需要basePackage
- * 3 可以使用@Configuration将类变成配置类
+ * 15
+ * 将主函数所在类，定义为@Configuration是个明智之举
+ * <p>
+ * 15.1
+ * 可以使用@Import包含多个配置类，而不需要将所有配置写在一个类当中。
+ * 否则，@ComponentScan可以包含所有Spring Components，包括@Configuration注解
+ * <p>
+ * 15.2
+ * 如果坚持使用xml配置文件的方式，我们任然推荐先有一个@Configuration,
+ * 然后使用@ImportResource注解加载该配置文件
  */
 @Configuration
 @EnableAutoConfiguration
